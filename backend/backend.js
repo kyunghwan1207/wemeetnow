2985c2a0cdb920af3c3d1d842ef885ab60377cd0
kyunghwan1207 <kyunghwanko1207@gmail.com>
2022-11-18 01:14:07 +0900

    Init: 초기프로젝트 구조 설정

---
+++ backend/backend.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const test = require("./router/test");
+app.use("/api", test);
+const port = 5000;
+app.listen(port, () => console.log(`Listen on .. ${port}`))
